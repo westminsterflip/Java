eaf5a3bed2dbe20928d17f969d223b69d8d404d0
patterson_ian <patterson_ian@cl.fcasd.edu>
2016-02-03 10:59:54 -0500

    .

---
+++ ISBN Program/src/ISBN_Program.java
@@ -0,0 +1,9 @@
+
+public class ISBN_Program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ test/src/test/test2.java
@@ -0,0 +1,10 @@
+package test;
+
+public class test2 {
+
+	public static void main(String[] args) {
+		String jay = "repeated numbers and letters";
+				System.out.print(jay.indexOf("t"));
+	}
+	
+}

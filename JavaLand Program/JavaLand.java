b4c695ce2ec1bf9f2a25755788ae776601f03940
patterson_ian <patterson_ian@cl.fcasd.edu>
2016-02-05 10:59:07 -0500

    Income

---
+++ JavaLand Program/JavaLand.java
@@ -0,0 +1,19 @@
+
+public class JavaLand {
+	
+	public class Taxes{
+		private int income;
+		
+		public Taxes(int inco){
+			income = inco;
+		}
+		public void setIncome(int inco){
+			income = inco;
+		}
+	}
+	
+	public static void main(String[] args) {
+		
+	}
+
+}

37c3f7c09e83fc1b8928074c7bcec9d2ec97baae
patterson_ian <patterson_ian@cl.fcasd.edu>
2016-02-05 09:54:38 -0500

    .

---
+++ test/src/test/Fraction.java
@@ -0,0 +1,15 @@
+package test;
+
+public class Fraction {
+	private int numerator;
+	private int denominator;
+	
+
+
+public Fraction(int num, int den){
+	numerator=num;
+	denominator=den;
+	
+}
+
+}
--- test/src/test/test2.java
+++ test/src/test/test2.java
@@ -1,8 +1,14 @@
 package test;
 
 public class test2 {
+	
+	public test2(){
+		
+	}
 
 	public static void main(String[] args) {
+		test2 thing = new test2();
+		test2 thing2 = new test2();
 		String jay = "repeated numbers and letters";
 				System.out.print(jay.indexOf("t"));
 	}

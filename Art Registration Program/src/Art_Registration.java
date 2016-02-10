d30af14ad8b2620b435ae766b8568c0f980dec5e
Ian P <irpatte@gmail.com>
2016-02-10 18:38:53 -0500

    sdfghjkl;'

--- Art Registration Program/src/Art_Registration.java
+++ Art Registration Program/src/Art_Registration.java
@@ -69,21 +69,56 @@ public class Art_Registration {
 	public int inputID()throws IOException{
 		InputStreamReader readerID = new InputStreamReader(System.in);
 		BufferedReader inputID = new BufferedReader(readerID);
+		boolean validIn = false;
+		int id = 0;
 		
-		System.out.print("Input ID code: ");
-		int id = Integer.parseInt(inputID.readLine());
+		while (validIn == false){
+			System.out.print("Input ID code: ");
+			String input1 = inputID.readLine();
+			if (input1.equals("")){
+				System.out.println("ID must be given");
+				validIn = false;
+			} else {
+				id = Integer.parseInt(input1);
+				validIn = true;
+			}
+		}
 		return id;
 	}
-	
-	public int inputCourse(){
+		
+	public int inputCourse() throws IOException{
 		InputStreamReader readerCourse = new InputStreamReader(System.in);
 		BufferedReader inputCourse = new BufferedReader(readerCourse);
-		String bBcCpPsSwW
+		String characters = "bBcCpPsSwW";
+		boolean validIn = false;
+		int coursenum = 0;
 		
-		do{
-		System.out.print("Input ID code: ");
-		String course = inputCourse.readLine();
+		while (validIn == false){
+			System.out.print("Input ID code: ");
+			String course = inputCourse.readLine();
+			if (course.equals("")){
+				validIn = false;
+				System.out.println("Course code must be given");
+			} else if (characters.indexOf(course) == -1){
+				System.out.println("Invalid course code");
+				validIn = false;
+			} else if (characters.indexOf(course) == 0 || characters.indexOf(course) == 1){
+				validIn = true;
+			} else if (characters.indexOf(course) == 2 || characters.indexOf(course) == 3){
+				validIn = true;
+				coursenum = 1;
+			} else if (characters.indexOf(course) == 4 || characters.indexOf(course) == 5){
+				validIn = true;
+				coursenum = 2;
+			} else if (characters.indexOf(course) == 6 || characters.indexOf(course) == 7){
+				validIn = true;
+				coursenum = 3;
+			} else if (characters.indexOf(course) == 8 || characters.indexOf(course) == 9){
+				validIn = true;
+				coursenum = 4;
+			}
 		}
+		return coursenum;
 	}
 
 }

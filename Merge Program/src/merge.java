7b9507b5a6a4e0fdb69babfe68a1e563d7acb8b0
Ian P <irpatte@gmail.com>
2016-03-08 23:59:48 -0500

    Slightly better

--- Merge Program/src/merge.java
+++ Merge Program/src/merge.java
@@ -2,50 +2,53 @@ import java.util.*;
 public class merge {
 	
 	public merge(){}
-	Scanner s1 = new Scanner(System.in);
 	ArrayList<Integer> b = new ArrayList<Integer>();
 	ArrayList<Integer> a = new ArrayList<Integer>();
+	ArrayList<Integer> a3 = new ArrayList<Integer>();
+	int t1,t2,c,d;
+	boolean onegood=false;
 	
-	public void array1In(){
+	public void arrayIn(){
+		Scanner s1 = new Scanner(System.in);
 		int x=0;
-		try{
-			System.out.print("Length of list one: ");
-			int len = s1.nextInt();
-			if(len<=0){
-				System.out.println("List length must be greater than 0");
-				s1.nextLine();
-				array1In();
-			}else{
-				System.out.print("Input Numbers: ");
-				for(x=0;x<len;x++){
-					a.add(s1.nextInt());
+		if(onegood==false){
+			try{
+				System.out.print("Length of list one: ");
+				int len = s1.nextInt();
+				if(len<=0){
+					System.out.println("List length must be greater than 0");
+					s1.nextLine();
+					arrayIn();
+				}else{
+					System.out.print("Input Numbers: ");
+					for(x=0;x<len;x++){
+						a.add(s1.nextInt());
+					}
+					onegood=true;
 				}
 			}
-		}
-		catch(InputMismatchException exception){
-			System.out.println("Input must be integers.");
-			s1.nextLine();
-			int d = a.size()-x;
-			for(int y=a.size()-1;y>=d;y--){
-				a.remove(y);
+			catch(InputMismatchException exception){
+				System.out.println("Input must be integers.");
+				s1.nextLine();
+				int d = a.size()-x;
+				for(int y=a.size()-1;y>=d;y--){
+					a.remove(y);
+				}
+				arrayIn();
+				
 			}
-			array1In();
-			
 		}
-	}
-	
-	public void array2In(){
-		int x=0;
+		x=0;
 		try{
 			System.out.print("Length of list two: ");
-			int len = s1.nextInt();
-			if(len<=0){
+			int len1 = s1.nextInt();
+			if(len1<=0){
 				System.out.println("List length must be greater than 0");
 				s1.nextLine();
-				array2In();
+				arrayIn();
 			}else{
 				System.out.print("Input Numbers: ");
-				for(x=0;x<len;x++){
+				for(x=0;x<len1;x++){
 					a.add(s1.nextInt());
 				}
 			}
@@ -57,86 +60,74 @@ public class merge {
 			for(int y=a.size()-1;y>=d;y--){
 				a.remove(y);
 			}
-			array2In();
+			arrayIn();
 		}
+		s1.close();
 	}
 	
-	public ArrayList<Integer> sortThose(){
-		ArrayList<Integer> a1 = a, a2 = b, a3 = new ArrayList<Integer>();
-		int t1 = 0, t2 = 0,pi=(int)(Math.PI*1000000);
-		int c,d;
-		if(a1.size()==1 && a2.size()==1){
-			if(a1.get(0)<a2.get(0)){
-				a3.add(a1.get(0));
-				a3.add(a2.get(0));
-			}else{
-				a3.add(a2.get(0));
-				a3.add(a1.get(0));
-			}}else{
-				for(int w3=0;w3!=pi;){
-					if(a1.isEmpty()==false){
-						t1=a1.get(0);
-						for(c=0;c< a1.size();c++){
-							if (t1>a1.get(c)){
-								t1 = a1.get(c);
-							}
-						}
+	public void sortThose(){
+		//System.out.println(a + " and "+ b);
+		if(a.isEmpty()==true&&b.isEmpty()==true)
+		{
+			godostuff();
+		}else{
+			if(a.isEmpty()==false){
+				t1=a.get(0);
+				for(c=0;c< a.size();c++){
+					if (t1>a.get(c)){
+						t1 = a.get(c);
 					}
-					
-					if(a2.isEmpty()==false){
-						t2 = a2.get(0);
-						for(d=0;d<a2.size();d++){
-							if (t2>a2.get(d)){
-								t2 = a2.get(d);
-							}
-						}
+				}
+			}
+			
+			if(b.isEmpty()==false){
+				t2 = b.get(0);
+				for(d=0;d<b.size();d++){
+					if (t2>b.get(d)){
+						t2 = b.get(d);
 					}
-				if(t1==t2&&(a1.indexOf(t1)!=-1||a2.indexOf(t2)!=-1)){
-					if(a1.indexOf(t1)!=-1){
-						a1.remove(a1.indexOf(t1));
-					}
-					if(a2.indexOf(t2)!=-1){
-						a2.remove(a2.indexOf(t2));
-					}
+				}
+			}
+			
+			if(t1==t2&&(a.indexOf(t1)!=-1||b.indexOf(t2)!=-1)){
+				if(a.indexOf(t1)!=-1){
+					a.remove(a.indexOf(t1));
+				}
+				if(b.indexOf(t2)!=-1){
+					b.remove(b.indexOf(t2));
+				}
+				if(a3.indexOf(t1)==-1){
+					a3.add(t1);
+				}
+			}else if((t1<t2||b.isEmpty())&&a.indexOf(t1)!=-1){
 					if(a3.indexOf(t1)==-1){
 						a3.add(t1);
 					}
-					w3+=2;
-				}else if((t1<t2||a2.isEmpty())&&a1.indexOf(t1)!=-1){
-					if(a3.indexOf(t1)==-1){
-						a3.add(t1);
-					}
-					a1.remove(a1.indexOf(t1));
-					w3++;
-				}else if ((t2<t1||a1.isEmpty())&&a2.indexOf(t2)!=-1){
+					a.remove(a.indexOf(t1));
+			}else if ((t2<t1||a.isEmpty())&&b.indexOf(t2)!=-1){
 					if(a3.indexOf(t2)==-1){
 						a3.add(t2);
 					}
-					w3++;
-					a2.remove(a2.indexOf(t2));
-				}else w3=pi;
+					b.remove(b.indexOf(t2));
 			}
+			sortThose();
 		}
-		
-		return a3;
 	}
 	
-	public String toString(){
-		array1In();
-		array2In();
-		s1.close();
+	public void godostuff(){
 		String output = '\r'+"";
-		ArrayList<Integer> a5 = sortThose();
-		for(int t=0;t < a5.size();t++){
-			output += a5.get(t) + " ";
+		for(int t=0;t < a3.size();t++){
+			output += a3.get(t) + " ";
 		}
-		return output;
+		a3.clear();
+		System.out.println(output);
 	}
 	
 	public static void main(String[] a){
 		merge m1 = new merge();
-		System.out.println(m1.toString());
-		
+		m1.arrayIn();
+		m1.sortThose();
+		m1.godostuff();
 	}
 	
 }

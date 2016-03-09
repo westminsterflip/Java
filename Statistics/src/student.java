2851ae0198d58209ed301822b32c0566ec21ab8a
patterson_ian <patterson_ian@cl.fcasd.edu>
2016-03-09 10:59:50 -0500

    started

---
+++ Statistics/src/student.java
@@ -0,0 +1,17 @@
+import java.io.*;
+
+public class student {
+	private String[] studen = new String[3];
+	
+	public student(){
+	}
+	
+	public String[] getStu(){
+		return studen;
+	}
+	
+	public void inFile(){
+		
+	}
+	
+}

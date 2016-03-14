6f775bcd37f42b127345b7696b86ce9243851000
Ian P <irpatte@gmail.com>
2016-03-13 21:24:27 -0400

    most working

--- Statistics/src/stats.java
+++ Statistics/src/stats.java
@@ -1,10 +1,11 @@
 import java.io.*;
+import java.util.Arrays;
 public class stats{
 	private student[] s1 = new student[30];
 	private int[] scr = new int[30];
 	private String file;
 	private String codes = "",in="";
-	int num=0,high=0,low=100,sum=0,count = 0,cnt1=0;
+	int num=0,sum=0,count = 0,cnt1=0;
 	
 	
 	public stats() throws IOException{
@@ -86,17 +87,12 @@ public class stats{
 				//System.out.println(strt + " and " + index);
 				//System.out.println(nice);
 				//System.out.println(nice.substring(0,inds1));
-			//	System.out.println(nice.substring(3,5));
+			  	System.out.println(nice.substring(3,5));
 				//System.out.println(nice.substring(6));
 				failure.setID(nice.substring(0,inds1).trim());
 				failure.setGrade(nice.substring(inds1,inds2).trim());
 				try{
 					int score = failure.getGrade();
-					if(score>high)
-						high=score;
-					if(score<low){
-						low = score;
-					}
 					scr[(int)count] = score;
 					count++;
 					sum+=score;
@@ -107,6 +103,7 @@ public class stats{
 				//System.out.println(getStudent(num).getStuff());
 				num++;
 				strt = index+3;
+				
 			}
 			catch(StringIndexOutOfBoundsException e){
 				//System.out.println("stopped trying");
@@ -128,46 +125,52 @@ public class stats{
 					failure.setGrade(nice.substring(inds1,inds2).trim());
 					failure.setMajor(nice.substring(inds2).trim());
 					s1[num] = failure;
+					try{
+						int score = failure.getGrade();
+						scr[(int)count] = score;
+						count++;
+						sum+=score;
+					}
+					catch(StringIndexOutOfBoundsException e2){}
+					num++;
 				}
 				catch(StringIndexOutOfBoundsException i){}
-				try{
-					int score = failure.getGrade();
-					if(score>high)
-						high=score;
-					if(score<low){
-						low = score;
-					}
-					scr[(int)count] = score;
-					count++;
-					sum+=score;
-				}
-				catch(StringIndexOutOfBoundsException e2){}
 				//System.out.println(getStudent(num).getStuff());
 			}
 		}
 	}
 
 	public void sort(){
-		int tmp=0,temp = 0;
+		int tmp=0,temp = 0,low;
 		student tp = new student(),tep = new student();
-		for(int x=0;x<count-1;x++){
-			
-			System.out.println(scr[x]+"and");
-			System.out.println(x);
+		for(int x=0;x<count;x++){
+			low=x;
 			tmp = scr[x];	
 			temp = tmp;
 			int y;
 			for(y=x+1;y<count;y++){
-				System.out.print(scr[y] + ",  , ");
 				if(scr[y]<tmp){
-					//System.out.println("changed");
 					tmp=scr[y];
+					low=y;
 				}
 			}
 			scr[x]=tmp;
-			scr[y]=temp;
+			scr[low]=temp;
 		}
-		
+		for(int x=0;x<num;x++){
+			low=x;
+			tp = s1[x];	
+			tep = tp;
+			int y;
+			for(y=x+1;y<num;y++){
+				if(s1[y].getID()<tp.getID()){
+					tp=s1[y];
+					low=y;
+				}
+			}
+			s1[x]=tp;
+			s1[low]=tep;
+		}
 	}
 	
 	public String toString(int ind){
@@ -178,19 +181,22 @@ public class stats{
 		inputter();
 		makeClass();
 		sort();
+		/*for(int x=0;x<30;x++){
+			System.out.println(scr[x]);
+		}*/
 		System.out.println("ID:  Grade:  Letter Grade:  Major:");
 		for(int x=0;s1[x]!=null;x++){
 			//System.out.println(x);
 			System.out.println(toString(x));
 		}
 		System.out.println("Max:  Min:  Mean:  Median:");
-		System.out.print(high+"%");
-		for(int x = (high+"").length();x<5;x++){
+		System.out.print(scr[count-1]+"%");
+		for(int x = (scr[count-1]+"").length();x<5;x++){
 			System.out.print(" ");
 		}
-		System.out.print(low+"%");
+		System.out.print(scr[0]+"%");
 		double mean = Math.round((double)sum/count*10)/10.0;
-		for(int x = (low+"").length();x<5;x++){
+		for(int x = (scr[0]+"").length();x<5;x++){
 			System.out.print(" ");
 		}
 		System.out.print(mean+"%");
@@ -198,9 +204,6 @@ public class stats{
 			System.out.print(" ");
 		}
 		System.out.println((scr[count/2]+scr[count-1-count/2])/2.0);
-		for(int x=0;x<count;x++){
-			System.out.println(scr[x]+ ", ");
-		}
 	}
 	
 	public static void main(String[] schience) throws IOException{

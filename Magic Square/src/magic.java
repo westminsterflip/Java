c234fe73b426bde8ade4303e23a2161f2ac61688
patterson_ian <patterson_ian@cl.fcasd.edu>
2016-03-14 10:50:42 -0400

    started

---
+++ Magic Square/src/magic.java
@@ -0,0 +1,23 @@
+import java.io.*;
+public class magic {
+	int[][] sqr;
+	int x;
+	
+	public magic(){
+	}
+	
+	public void in() throws IOException{
+		InputStreamReader in = new InputStreamReader(System.in);
+		BufferedReader li = new BufferedReader(in);
+		x = Integer.parseInt(li.readLine());
+		System.out.print("Order: ");
+		sqr = new int[x][x];
+	}
+	
+	public void even(){
+		for(int y = 0;y<x;y++){
+			
+		}
+	}
+	
+}

310650fe26767a8f580837986aebbf6c950733fd
patterson_ian <patterson_ian@cl.fcasd.edu>
2016-03-16 10:08:02 -0400

    started

---
+++ Implementation/src/elstudent.java
@@ -0,0 +1,57 @@
+
+public class elstudent implements student{
+	
+	private String fi;
+	private String la;
+	private int gr;
+	private String in;
+	
+	public elstudent(){
+	}
+	
+	public elstudent(String a){
+		in = a;
+		fi = in.substring(0, in.indexOf(' '));
+		la = in.substring(in.indexOf(' ',in.indexOf(' ')+1)).trim();
+		gr = Integer.parseInt(in.substring(in.indexOf(' ',in.indexOf(' ')+1)).trim());
+		System.out.println(toString());
+	}
+	
+	public void setIn(String a){
+		in = a;
+	}
+	
+	public String getIn(){
+		return in;
+	}
+	
+	public void setGLevel(int a) {
+		gr = a;
+	}
+
+	public String getFName() {
+		return fi;
+	}
+
+	public String getLName() {
+		return la;
+	}
+	
+	public String toString(){
+		String output = la + ", " + fi + "  " + gr;
+		return output;
+	}
+
+	@Override
+	public void setFName(String a) {
+		fi = a;
+	}
+
+	public void setLName(String a) {
+		la = a;
+	}
+
+	public int getGLevel() {
+		return gr;
+	}
+}
---
+++ Implementation/src/student.java
@@ -0,0 +1,9 @@
+
+public interface student {
+	void setFName(String a);
+	void setLName(String a);
+	void setGLevel(int a);
+	String getFName();
+	String getLName();
+	int getGLevel();
+}

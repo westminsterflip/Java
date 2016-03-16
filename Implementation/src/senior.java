539a4538dd88f3faf0e7dad4006b3f6b9e82ce7e
patterson_ian <patterson_ian@cl.fcasd.edu>
2016-03-16 10:55:36 -0400

    students

---
+++ Implementation/src/senior.java
@@ -0,0 +1,41 @@
+
+public class senior extends hsstu{
+	private double fine;
+	private boolean podone;
+	
+	public void setFName(String a) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setLName(String a) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setGLevel(int a) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public String getFName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getLName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public int getGLevel() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public int compareTo(String o) {
+		String e = getGLevel() + 
+		return ;
+	}
+
+}
---
+++ Implementation/src/sophmore.java
@@ -0,0 +1,46 @@
+
+public class sophmore extends hsstu{
+
+	@Override
+	public void setFName(String a) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setLName(String a) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setGLevel(int a) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String getFName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getLName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getGLevel() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int compareTo(String o) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}

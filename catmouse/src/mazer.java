fc57c9f44cd962dcba0b2a0ce48b743c5d36cd45
Ian P <irpatte@gmail.com>
2016-04-05 22:10:31 -0400

    progress

--- catmouse/src/mazer.java
+++ catmouse/src/mazer.java
@@ -9,9 +9,10 @@ public class mazer extends JFrame implements ActionListener,MouseListener{
 	JPanel maz = new JPanel();
 	ArrayList<JPanel> blocs = new ArrayList<JPanel>(); 
 	String in;
-	ArrayList<JLabel> name = new ArrayList<JLabel>();
+	ArrayList<String> name = new ArrayList<String>();
 	JPanel kat = new JPanel();
-	JLabel ca=new JLabel(new ImageIcon("cat.png"));
+	JLabel ca=new JLabel(new ImageIcon("cat.png")), paw = new JLabel(new ImageIcon("paws.png"));
+	JLabel maus=new JLabel(new ImageIcon("mouse.png")) ,blnk = new JLabel("");
 	int len = 3, wid = 3,mice=0,cats=0;
 	//JTextField le = new JTextField(3);
 	//JTextField wi = new JTextField(3);
@@ -33,6 +34,7 @@ public class mazer extends JFrame implements ActionListener,MouseListener{
 	JMenuItem nwi = new JMenuItem("New Custom");
 	JPanel mazens = new JPanel();
 	JPanel menubar = new JPanel();
+	int index;
 	
 	
 	public mazer(){
@@ -53,13 +55,13 @@ public class mazer extends JFrame implements ActionListener,MouseListener{
 		menubar.add(menu);
 		add(menu,BorderLayout.NORTH);
 		
-		maz.setPreferredSize(new Dimension(60,60));
+		maz.setPreferredSize(new Dimension(75,75));
 		//maz.setSize(new Dimension(60,60));
 		maz.setLayout(new FlowLayout(FlowLayout.CENTER, 0, 0));
 		blocs.add(kat);
-		name.add(ca);
+		name.add("C");
 		kat.add(ca);
-		kat.setPreferredSize(new Dimension(20,20));
+		kat.setPreferredSize(new Dimension(25,25));
 		kat.setBackground(Color.yellow);
 		kat.setOpaque(true);
 		kat.addMouseListener(this);
@@ -78,21 +80,23 @@ public class mazer extends JFrame implements ActionListener,MouseListener{
 		//bot.add(but, BorderLayout.SOUTH);
 		for(int x = 1;x<9;x++){
 			JPanel j = new JPanel();
-			JLabel f = new JLabel("");
-			j.setPreferredSize(new Dimension(20,20));
+			//JLabel f = new JLabel("");
+			j.setPreferredSize(new Dimension(25,25));
 			j.setOpaque(true);
 			j.setBackground(Color.black);
-			j.add(f);
+			//j.add(f);
 			//j.setBorder(BorderFactory.createLineBorder(Color.black,1));
 			maz.add(j);
 			blocs.add(j);
-			name.add(f);
+			name.add("");
 			blocs.get(x).addMouseListener(this);
 		}
 		blocs.get(8).setBackground(Color.cyan);
 		name.remove(8);
-		name.add(new JLabel(new ImageIcon("mouse.png")));
-		blocs.get(8).add(name.get(8));
+		//name.add(new JLabel(new ImageIcon("mouse.png")));
+		//blocs.get(8).add(new JLabel(new ImageIcon("mouse.png")));
+		blocs.get(8).add(maus);
+		name.add("M");
 		mazens.add(maz);
 		nwe.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_N, KeyEvent.CTRL_DOWN_MASK));
 		nwi.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_N, KeyEvent.CTRL_DOWN_MASK + KeyEvent.SHIFT_DOWN_MASK));
@@ -100,6 +104,7 @@ public class mazer extends JFrame implements ActionListener,MouseListener{
 		goc.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_R, KeyEvent.CTRL_DOWN_MASK + KeyEvent.SHIFT_DOWN_MASK));
 		//mazens.add(bot);
 		add(mazens,BorderLayout.CENTER);
+		//blocs.get(8).getComponent(1);
 		//go.addActionListener(this);
 		//nw.addActionListener(this);
 		//setPreferredSize(new Dimension(50,50))
@@ -131,79 +136,78 @@ public class mazer extends JFrame implements ActionListener,MouseListener{
 		setLayout(new BorderLayout());
 		menubar.add(menu);
 		add(menu,BorderLayout.NORTH);
-		maz.setPreferredSize(new Dimension(w*20,l*20));
+		maz.setPreferredSize(new Dimension(w*25,l*25));
 		maz.setLayout(new FlowLayout(FlowLayout.CENTER, 0, 0));
 		blocs.add(kat);
-		name.add(ca);
+		name.add("C");
 		kat.add(ca);
-		kat.setPreferredSize(new Dimension(20,20));
+		kat.setPreferredSize(new Dimension(25,25));
 		kat.setBackground(Color.yellow);
 		kat.setOpaque(true);
 		kat.addMouseListener(this);
-		gon.addActionListener(this);
 		//kat.setBorder(BorderFactory.createLineBorder(Color.black,1));
 		maz.add(kat);
-		menubar.setLayout(new BoxLayout(menubar,BoxLayout.X_AXIS));
-		//mazens.setLayout(new FlowLayout(FlowLayout.LEADING, 0, 0));
-		//setLayout(new FlowLayout(FlowLayout.CENTER, 0, 0));
-		menubar.add(menu);
-		add(menu,BorderLayout.NORTH);
-		for(int x = 1;x<w*l;x++){
-			JPanel j = new JPanel();
-			JLabel f = new JLabel("");
-			j.setPreferredSize(new Dimension(20,20));
-			j.setOpaque(true);
-			j.setBackground(Color.black);
-			j.add(f);
-			//j.setBorder(BorderFactory.createLineBorder(Color.black,1));
-			maz.add(j);
-			blocs.add(j);
-			name.add(f);
-			blocs.get(x).addMouseListener(this);
-		}
-	//	neu.setLayout(new BoxLayout(neu,BoxLayout.X_AXIS));
-	//	neu.add(wi);
-	//	neu.add(by);
-	//	neu.add(le);
+		gon.addActionListener(this);
+		//neu.setLayout(new BoxLayout(neu,BoxLayout.X_AXIS));
+		//neu.add(wi);
+		//neu.add(by);
+		//neu.add(le);
 		//but.setLayout(new BoxLayout(but,BoxLayout.X_AXIS));
 		//but.add(go);
 		//but.add(nw);
 		//bot.setLayout(new BorderLayout());
 		//bot.add(neu,BorderLayout.NORTH);
 		//bot.add(but, BorderLayout.SOUTH);
+		for(int x = 1;x<w*l;x++){
+			JPanel j = new JPanel();
+			//JLabel f = new JLabel("");
+			j.setPreferredSize(new Dimension(25,25));
+			j.setOpaque(true);
+			j.setBackground(Color.black);
+			//j.add(f);
+			//j.setBorder(BorderFactory.createLineBorder(Color.black,1));
+			maz.add(j);
+			blocs.add(j);
+			name.add("");
+			blocs.get(x).addMouseListener(this);
+		}
 		blocs.get(blocs.size()-1).setBackground(Color.cyan);
-		name.get(blocs.size()-1).setText("M");
+		name.add("M");
+		//name.remove(8);
+		//name.add(new JLabel(new ImageIcon("mouse.png")));
+		blocs.get(blocs.size()-1).add(new JLabel(new ImageIcon("mouse.png")));
+		name.add("M");
 		mazens.add(maz);
-		//mazens.add(bot);
-		add(mazens,BorderLayout.CENTER);
 		nwe.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_N, KeyEvent.CTRL_DOWN_MASK));
 		nwi.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_N, KeyEvent.CTRL_DOWN_MASK + KeyEvent.SHIFT_DOWN_MASK));
 		gon.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_R, KeyEvent.CTRL_DOWN_MASK));
 		goc.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_R, KeyEvent.CTRL_DOWN_MASK + KeyEvent.SHIFT_DOWN_MASK));
+		//mazens.add(bot);
+		add(mazens,BorderLayout.CENTER);
+		//blocs.get(8).getComponent(1);
 		//go.addActionListener(this);
 		//nw.addActionListener(this);
-		//setPreferredSize(new Dimension(50,50));
+		//setPreferredSize(new Dimension(50,50))
+		//setMaximumSize(new Dimension(wid+5,len+5));
+		//setSize(new Dimension(wid+5,len+20));
 		pack();
 		setVisible(true);
-		//System.out.println(maz.getHeight() + " " + blocs[0][0].getHeight() + " " + getWidth());
-		//System.out.println(w + " " + l);
-		len=l;
-		wid=w;
-		//System.out.println(wid + " " + len);
 		int wifd = (int)Math.round((scr.getWidth()-getWidth())/2);
 		int hig = (int)Math.round((scr.getHeight()-getHeight())/2);
 		setLocation(wifd,hig);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		//System.out.println(maz.getHeight() + " " + blocs.get(0).getHeight() + " " + getWidth());
 	}
 
 	public void counter(){
 		cats=0;
 		mice=0;
-		for(int z = 0;z<name.size();z++){
-			if(name.get(z).getText().equals("C"))
-				cats++;
-			else if(name.get(z).getText().equals("M"))
+		for(int z = 0;z<blocs.size();z++){
+			if(name.get(z)=="M"){
 				mice++;
+			}else if(name.get(z)=="C"){
+				cats++;
+			}
 		}
 	}
 	
@@ -251,17 +255,29 @@ public class mazer extends JFrame implements ActionListener,MouseListener{
 		if(e.getComponent().getBackground().equals(Color.white)){
 			e.getComponent().setBackground(Color.yellow);
 			//setCat(k);
-			setName(k,"C");
+			name.remove(blocs.indexOf(k));
+			name.add(blocs.indexOf(k), "C");
+			k.removeAll();
+			k.add(ca);
 		}else if(e.getComponent().getBackground().equals(Color.yellow)){
 			e.getComponent().setBackground(Color.cyan);
 			//setMouse(k);
-			setName(k,"M");
+			name.remove(blocs.indexOf(k));
+			name.add(blocs.indexOf(k), "M");
+			k.removeAll();
+			k.add(maus);
 		}else if(e.getComponent().getBackground().equals(Color.cyan)){
 			e.getComponent().setBackground(Color.black);
-			setName(k,"");
+			name.remove(blocs.indexOf(k));
+			name.add(blocs.indexOf(k), "");
+			k.removeAll();
+			k.add(blnk);
 		}else if(e.getComponent().getBackground().equals(Color.black)){
 			e.getComponent().setBackground(Color.white);
-			setName(k,"");
+			k.removeAll();
+			k.add(blnk);
+			name.remove(blocs.indexOf(k));
+			name.add(blocs.indexOf(k), "");
 		}
 		
 	}
@@ -272,7 +288,7 @@ public class mazer extends JFrame implements ActionListener,MouseListener{
 		for(int d=0;d<blocs.size();d++){
 			if(blocs.get(d).getBackground().equals(Color.black)){
 				in+="#";
-			}else if(blocs.get(d).getBackground().equals(Color.yellow)&&name.get(d).getText().equals("C")){
+			}else if(blocs.get(d).getBackground().equals(Color.yellow)&&name.get(d).equals("C")){
 				in+="C";
 			}else if(blocs.get(d).getBackground().equals(Color.yellow)){
 				in+=" ";
@@ -284,37 +300,50 @@ public class mazer extends JFrame implements ActionListener,MouseListener{
 		}
 		//System.out.println(in);
 		maze_2 m2 = new maze_2(in,wid,len);
-		//System.out.println(m2.toString());
+		System.out.println(m2.toString());
 		maz.setVisible(false);
 		translate(m2.gitDun());
 		//translate(m2.gitDun());
 		maz.setVisible(true);
+		name.clear();
 	}
 	
-	public void setName(JPanel h, String u){
-		name.get(blocs.indexOf(h)).setText(u);
-	}
+	//public void setName(JPanel h, String u){
+		
+	//}
 	
 	public void translate(char[][] ma){
 		int us = 0;
 		for(int f=0;f<len;f++){
 			for(int s=0;s<wid;s++){
-				//System.out.println(s + " " + f + " " + ma[s][f]);
+				System.out.println(s + " " + f + " " + ma[s][f]);
 				switch(ma[s][f]){
 				case 'C': blocs.get(us).setBackground(Color.yellow);
-						name.get(us).setText("C");
+				blocs.get(us).removeAll();		
+				blocs.get(us).add(new JLabel(new ImageIcon("cat.png")));
+				name.remove(us);
+				name.add(us, "C");
 						break;
 				case ' ': blocs.get(us).setBackground(Color.white);
-				name.get(us).setText("");
+				blocs.get(us).removeAll();
+				//blocs.get(us).add(blnk);
 						break;
 				case 'M': blocs.get(us).setBackground(Color.cyan);
-				name.get(us).setText("M");
+					blocs.get(us).removeAll();		
+					blocs.get(us).add(new JLabel(new ImageIcon("mouse.png")));
+					name.remove(us);
+					name.add(us, "C");
 						break;
-				case '#': blocs.get(us).setBackground(Color.black);
-				name.get(us).setText("");
-						break;
+				//case '#': blocs.get(us).setBackground(Color.black);
+				//blocs.get(us).removeAll();
+				//blocs.get(us).add(blnk);
+						//break;
 				case '+': blocs.get(us).setBackground(Color.yellow);
-				name.get(us).setText("");
+				blocs.get(us).removeAll();		
+				blocs.get(us).add(new JLabel(new ImageIcon("paws.png")));
+				System.out.println("blank");
+				//blocs.get(us).removeAll();
+				//blocs.get(us).add(blnk);
 						break;
 				}
 				us++;

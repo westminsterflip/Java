dc42b2633e578c9119c68bc24c39ba4feca6df9c
patterson_ian <patterson_ian@cl.fcasd.edu>
2016-04-18 10:34:17 -0400

    fork

---
+++ forktime/src/fork.java
@@ -0,0 +1,30 @@
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+@SuppressWarnings("serial")
+public class fork extends JFrame{
+	Dimension scr = Toolkit.getDefaultToolkit().getScreenSize();
+	
+	public fork(){
+		super("FOrkS and FOKRKDS AND FORKS  ADSND F0rk5");
+		JLabel squares = new JLabel("You wanna not?");
+		squares.setFont (squares.getFont ().deriveFont (150.0f));
+		setLayout(new BorderLayout());
+		add(squares, BorderLayout.CENTER);
+		setSize(scr);
+		setVisible(true);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+	}
+	
+	@SuppressWarnings("unused")
+	public static void main(String[] saltysaltysalty){
+		for(int y = 0;y<1;y++){
+			fork f = new fork();
+		}
+	}
+
+}

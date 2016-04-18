98273ecded059321a7edc3682dbc577d072dde70
Ian P <irpatte@gmail.com>
2016-04-18 17:39:00 -0400

    h

--- Monopoly/src/board.java
+++ Monopoly/src/board.java
@@ -1,9 +1,13 @@
 import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Toolkit;
 import java.awt.event.ComponentEvent;
 import java.awt.event.ComponentListener;
 import java.util.ArrayList;
-
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class board extends JFrame implements ComponentListener{
@@ -12,33 +16,47 @@ public class board extends JFrame implements ComponentListener{
 	JPanel right = new JPanel();
 	JPanel left = new JPanel();
 	JPanel center = new JPanel();
+	JPanel board = new JPanel();
+	JPanel shell = new JPanel();
 	ArrayList<JPanel> upp = new ArrayList<JPanel>();
 	ArrayList<JPanel> low = new ArrayList<JPanel>();
 	ArrayList<JPanel> rig = new ArrayList<JPanel>();
 	ArrayList<JPanel> lef = new ArrayList<JPanel>();
 	ArrayList<JPanel> cen = new ArrayList<JPanel>();
-	int xdim = 0, ydim = 0;
-	
+	//ArrayList<JPanel> all = new ArrayList<JPanel>();
+	Dimension scr = Toolkit.getDefaultToolkit().getScreenSize();
+	public int xpos = (int) scr.getWidth(),ypos = (int) scr.getHeight();
 	
 	public board(){
 		super("Monopoly");
-		setLayout(new BorderLayout());
-		
-		add(upper, BorderLayout.NORTH);
-		add(lower, BorderLayout.SOUTH);
-		add(right, BorderLayout.WEST);
-		add(left, BorderLayout.EAST);
-		add(center, BorderLayout.CENTER);
+		setLayout(new FlowLayout());
+		board.setLayout(new FlowLayout());
+		board.add(upper);
+		board.add(left);
+		board.add(center);
+		board.add(lower);
+		board.add(right);
+		add(board);
+		board.setPreferredSize(new Dimension(230,230));
+		board.setOpaque(true);
+		board.setBackground(Color.BLUE);
+		//board.add(new JLabel("HI"));
+		setMinimumSize(new Dimension(136,170));
 		setVisible(true);
 		pack();
-		xdim = getWidth();
-		ydim = getHeight();
+		addComponentListener(this);
+		setLocation((int)Math.round((xpos-getWidth())/2),(int)Math.round((ypos-getHeight())/2));
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
 	}
 	
 	@Override
 	public void componentResized(ComponentEvent e) {
-		// TODO Auto-generated method stub
-		
+		int multi = getWidth()-10;
+		System.out.println(getWidth() + " " + getHeight());
+		if(getHeight()<getWidth())
+			multi = getHeight()-10;
+		System.out.println("x " + multi);
+		board.setPreferredSize(new Dimension(multi,multi));
 	}
 
 	@Override
@@ -58,5 +76,10 @@ public class board extends JFrame implements ComponentListener{
 		// TODO Auto-generated method stub
 		
 	}
-
+	
+	public static void main(String[] args){
+		//TODO Science has occurred
+		board b1 = new board();
+	}
+	
 }

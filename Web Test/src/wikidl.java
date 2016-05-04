8ce775db1d052ccd810a83f2212b4457c0e06c26
patterson_ian <patterson_ian@cl.fcasd.edu>
2016-05-04 10:13:37 -0400

    c

--- Web Test/src/wikidl.java
+++ Web Test/src/wikidl.java
@@ -12,7 +12,7 @@ public class wikidl {
 		for(int y = 0;y<217;y++){
 			let.add(0);
 		}
-		while(let.get(216)<256){
+		while(let.get(216)<126){
 			InputStreamReader o = new InputStreamReader(System.in);
 			BufferedReader b = new BufferedReader(o);
 			//b.readLine();
@@ -23,25 +23,28 @@ public class wikidl {
 			}
 			let.set(til, let.get(til)+1);
 			for(int yu = 216;yu>=0;yu--){
-				if(let.get(yu).equals(256)){
+				if(let.get(yu).equals(126)){
 					if(yu==0){
 						System.out.println(let);
 						for(int i = 0;i<=til;i++){
-							let.set(i, 1);
+							let.set(i, 33);
 						}
-						let.set(til+1, 1);
+						let.set(til+1, 33);
 					}else{
-						let.set(yu, 1);
+						let.set(yu, 33);
 						let.set(yu-1, let.get(yu-1)+1);
 					}
 				}
 			}
-			//for(int yu = 0;yu<217;yu++){
-			//	System.out.print((char)(int)let.get(yu));
+			for(int yu = 0;yu<217;yu++){
+				System.out.print((char)(int)let.get(yu));
+			}
+			System.out.println();
+			//if(let.get(0).equals(73)&&let.get(1).equals(97)&&let.get(2).equals(110)){
+			//	let.set(216, 126);
 			//}
-			//System.out.println();
 		}
-		System.out.println(let);
+		//System.out.println(let);
 	}
 	
 	public static void main(String[] franny) throws IOException{

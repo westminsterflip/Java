4858178dbf24d2e9107c9ee5666776d4924b8eb2
Ian P <irpatte@gmail.com>
2016-10-28 14:48:27 -0400

    known wikis updated, flnameset created

---
+++ Web Test/src/flnameset.java
@@ -0,0 +1,19 @@
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class flnameset extends JFrame implements ActionListener{
+	JPanel win = new JPanel();
+	Dimension scr = Toolkit.getDefaultToolkit().getScreenSize();
+	JTextField pthbx = new JTextField(30);
+	
+	public void actionPerformed(ActionEvent e) {
+		
+	}
+	
+}
